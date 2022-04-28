7435cbaaeadf5b012fde24f7b7e0b31fca221f97
Szlendakooo <jacob.szlendak@spoko.pl>
2022-04-28 11:28:00 +0200

    0.2

--- generate.php
+++ generate.php
@@ -1,5 +1,58 @@
 <?php
     require_once 'vendor/autoload.php';
     $faker = Faker\Factory::create();
-    echo $faker->name;
+    $servername = "localhost";
+
+
+
+ $username = "root";
+
+
+
+ $password = "";
+
+
+
+ $dbname = "szlendakooo";
+
+
+
+ 
+
+
+
+ $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+
+ 
+
+
+
+ if (!$conn) {
+
+
+
+   die("Connection failed: " . mysqli_connect_error());
+
+
+
+ }
+
+
+
+
+
+ $name = $faker->name();
+$sql = "INSERT INTO images (`name`, `picsum_id`, `imagefile`, `author`, `width`, `height`, `added_at`) VALUES ('value-1','value-2','value-3','value-4','value-5','value-6','value-7')";
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+
+
+
+ }
+
+
 ?>
